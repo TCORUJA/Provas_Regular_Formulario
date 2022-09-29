05433874b1a5e742fd2e3d1f2b9d03a1ebcd7aaa
TCORUJA <113937074+TCORUJA@users.noreply.github.com>
2022-09-29 18:13:03 -0300

    Add files via upload

--- Links Provas Google/script.js
+++ Links Provas Google/script.js
@@ -1,14 +1,29 @@
-var exibirProva = document.getElementById('exibirProva');
+function confirmar(){
+    var senha = document.getElementById('senha').value;
+    var arr = senha.split(" ");
+    var camposenha = arr[0];
 
-$('#COB1084').click(function(){
-    exibirProva.src = "https://docs.google.com/forms/d/e/1FAIpQLSdEqpEGcNh11mzoEFH_YelBk_A__sCM5W78HlEtJrkuS7W0PQ/viewform";
+    var senhaDefinida = "123"
 
-    
+    if(camposenha == senhaDefinida){
     var autenticacao = document.getElementById('autenticacao');
     autenticacao.className = "autenticacaoOcultar";
     
     var links = document.getElementById('links');
-    links.className = "linksOcultar";
+    links.className = "linksExibir";
+    }
+
+}
+
+
+
+var exibirProva = document.getElementById('exibirProva');
+
+$('#COB1084').click(function(){
+    exibirProva.src = "https://docs.google.com/forms/d/e/1FAIpQLSdEqpEGcNh11mzoEFH_YelBk_A__sCM5W78HlEtJrkuS7W0PQ/viewform";
+    
+    var links = document.getElementById('links');
+    links.className = "links";
 
     var prova = document.getElementById('prova');
     prova.className = "provaExibir";
